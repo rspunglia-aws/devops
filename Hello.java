a1ce9532bf5b2cdd09acdedaf94ad0c30128e8eb
Ravinder Singh <rspunglia@gmail.com>
2020-11-02 23:08:24 +0000

    Added 2nd line of code

--- Hello.java
+++ Hello.java
@@ -1,2 +1,6 @@
-//This is new file with no code in it.
-// 
+class Simple{  
+ public static void main(String args[]){  
+        System.out.println("Hello Java");  
+        System.out.println("mY 2nd Change");
+	 }  
+ }  

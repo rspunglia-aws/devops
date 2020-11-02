4d644603420320c05b31742f26af6473f6d8ba85
Ravinder Singh <rspunglia@gmail.com>
2020-11-02 23:27:16 +0000

    Good code

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ class Simple{
  public static void main(String args[]){  
         System.out.println("Hello Java");  
         System.out.println("mY 2nd Change");
-//Comment 1
+//Comment 
+//dfdfdfdf
  }  
  }  
